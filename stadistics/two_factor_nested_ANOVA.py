c390fa9b5bfbbab9ebe203f92d68af93c426b815
Aaron1917 <aaron1917@users.noreply.github.com>
2024-10-23 17:19:46 -0600

    Update neasted anova means funtions
    Añadí funciones para:
    Cálculo de media Total.
    Cálculo de media de grupos A.
    Cálculo de media de grupos B en A.

--- stadistics/two_factor_nested_ANOVA.py
+++ stadistics/two_factor_nested_ANOVA.py
@@ -39,8 +39,39 @@ class Nested2FactorAnava():
         self.no_elements = [[len(e) for e in sublist] for sublist in elements]
         self.alpha = alpha
 
+    def calcular_media(self, data: pd.DataFrame):
+        return np.mean(data)
 
-        
+    def calcular_media_grupoA(self, data: pd.DataFrame, grupos_A: list):
+        results = []
+        for a in grupos_A:
+            filtro = (data[self.headers[0]] == a)
+            promedio = self.calcular_media(data[filtro][self.headers[2]])
+            results.append({self.headers[0] : a,'Promedio': promedio})
+        return pd.DataFrame(results)
+    
+    def calcular_media_grupoB(self, data: pd.DataFrame, grupos_A: list, grupos_B: list):
+        results = []
+        for i, a in enumerate(grupos_A):
+            for b in grupos_B[i]:
+                filtro = (self.data[self.headers[0]] == a) & (self.data[self.headers[1]] == b)
+                promedio = self.calcular_media(data[filtro][self.headers[2]])
+                results.append({self.headers[0] : a, self.headers[1] : b,'Promedio' : promedio})
+        return pd.DataFrame(results)
+
+    def calcular_means(self):
+        self.media_total = self.calcular_media(self.data[self.headers[2]])
+        self.media_grupoA = self.calcular_media_grupoA(self.data, self.grupos_A)
+        self.media_grupoB = self.calcular_media_grupoB(self.data, self.grupos_A, self.grupos_B)
+
+        print(f"La media total es: \n{self.media_total}")
+        print(f"La media de grupos A es: \n{self.media_grupoA}")
+        print(f"La media de grupos B es: \n{self.media_grupoB}")
+
+    def calculate_anova_table(self):
+        self.calcular_means()
+
+    
 
 if __name__ == "__main__":
     print("test")
@@ -58,4 +89,5 @@ if __name__ == "__main__":
     }
     datos = pd.DataFrame(example)
     anova_nested = Nested2FactorAnava(data=datos)
+    anova_nested.calcular_means()
     #print(anov)
